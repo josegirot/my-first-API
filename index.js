036e5ea13862129330d32b91b98e3afa6456201f
José Girot <josegirot20@gmail.com>
2022-04-19 18:20:09 -0500

    debug

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const app = express()
 app.use(cors())
 app.use(express.json())     
 
-const notes =[
+let notes =[
     {
       "userId": 1, 
       "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      "title": "Frutas",
+      "body": "Manzana"
     },
     {
       "userId": 1,
       "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+      "title": "fruta",
+      "body": "Pera"
     },
     {
       "userId": 1,
       "id": 3,
-      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+      "title": "fruta",
+      "body": "fresa"
     }]
 
 app.get('/',(request, response)=>{
@@ -78,8 +78,11 @@ app.post('/api/notes',(request, response)=>{
    const newNote =[{
     "userId": 1, 
     "id": maxId + 1,
+    "title": "new note",
     "body": note.body
    }]
+
+   notes = notes.concat(newNote)
 
    response.status(201).json(newNote)
 })
